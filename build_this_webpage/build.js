2695aa64b22c27ae1500ed5fc662f3a7c91c8dde
hmjatt <hmjatt@gmail.com>
2021-06-22 00:42:42 -0700

    work in progress

---
+++ build_this_webpage/build.js
@@ -0,0 +1,8 @@
+// const card = document.querySelector(".card")
+// const mainLink = document.querySelector('.main-link')
+
+// card.addEventListener("click", handleClick)
+
+// function handleClick(event) {
+//   mainLink.click();
+// }

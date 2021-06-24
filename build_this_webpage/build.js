7d48f116fcc7ecf1d017a67789f736c168e359af
hmjatt <hmjatt@gmail.com>
2021-06-24 02:13:09 -0700

    built header for google search results page

--- build_this_webpage/build.js
+++
@@ -1,8 +0,0 @@
-// const card = document.querySelector(".card")
-// const mainLink = document.querySelector('.main-link')
-
-// card.addEventListener("click", handleClick)
-
-// function handleClick(event) {
-//   mainLink.click();
-// }
